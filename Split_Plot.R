ed8774aa4bc7cffe9c74e15cf525001ca0922882
Brian_Buitrago <122131144+Bredbuca@users.noreply.github.com>
2025-08-23 15:58:40 -0500

    Update Split_Plot.R

--- Split_Plot.R
+++ Split_Plot.R
@@ -102,3 +102,95 @@ print(HSD.test(splitPdes$y,splitPdes$short,2, 0.084))
 print(HSD.test(splitPdes$y,splitPdes$bakeT,10, 0.098))
 
 print(HSD.test(splitPdes$y,splitPdes$trayT,10, 0.098))
+
+
+
+#anovas split plot complatamente aleatorizada Casella 2010 DIETAS HIPERTENSION
+#Con paquete aov
+library(daewr)
+data(splitPdes)
+names(splitPdes)
+model_1<-aov(y ~short+Error(short:batch)+short*BakeT*TrayT,data=splitPdes)
+summary(model_1)
+str(splitPdes)
+splitPdes
+
+# Verificacion Bondad de ajuste
+#variacion parcela principal
+cv<-100*sqrt(0.084)/mean(splitPdes$y)
+cv
+#variacion subparcela
+cv2<-100*sqrt(0.0098)/mean(splitPdes$y)
+cv2
+
+#validacion de supuestos
+# en arreglo de parcela principal
+mod_1_pp<-aov(y ~ short+Error(short:batch), data=splitPdes)
+summary(mod_1_pp)# El modelo se reparametriza.
+qqnorm(residuals(mod_1_pp))
+bartlett.test(splitPdes$y~splitPdes$short)
+plot(fitted(mod_1_pp))
+
+#unicamente para ver residuales, porque no se esta teniendo encuenta
+# el error.
+mod_0<-aov(splitPdes$y~splitPdes$short)
+plot(residuals(mod_0))
+library(MASS)
+boxcox(mod_0)
+hist(residuals(mod_0))
+shapiro.test(residuals(mod_0))
+#quedando sin el modelo sin transformar
+interaction.plot(splitPdes$short, splitPdes$trayT, splitPdes$y, mean)
+
+
+
+library(agricolae)
+#prueba de tukey para parcela principall
+print(HSD.test(splitPdes$y,splitPdes$short,2, 0.084))
+
+#prueba de tukey para temperatura del horno
+print(HSD.test(splitPdes$y,splitPdes$bakeT,10, 0.098))
+
+print(HSD.test(splitPdes$y,splitPdes$trayT,10, 0.098))
+
+
+
+# parcelas divididas, analisis mediante modelos mixtos
+anova(model_1)
+summary(model_1)
+
+#test f manual
+1-pt(0.1026/0.0004,1,2)
+
+model2<-lmer(y ~ 1  + bakeT + trayT + short:bakeT +
+               short:trayT + bakeT:trayT + short:bakeT:trayT +
+             (1|short:batch), data = splitPdes, REML = FALSE)
+model3<-lmer(y ~ 1 + short + bakeT + trayT + short:bakeT +
+               short:trayT + bakeT:trayT + short:bakeT:trayT +
+               (1|short:batch), data = splitPdes, REML = FALSE)
+
+
+#parcelas divididas arregladas en bloques
+#casella
+library(readxl)
+var.alfalfa <- read.table("C:/Users/Tuptc/Downloads/VarietySP.txt", h=T)
+View(var.alfalfa) 
+
+#ANÁLISIS EXPLORATORIO
+
+#Numérico
+head(var.alfalfa)
+str(var.alfalfa)
+summary(var.alfalfa)
+attach(var.alfalfa)
+
+boxplot(Y~Variety,data = var.alfalfa)
+boxplot(Y~Trt,data = var.alfalfa)
+
+
+#ANALISIS ESTADISTICO
+p<-as.factor(var.alfalfa$P)
+k<-as.factor(var.alfalfa$K)
+
+modalf<-aov(Y~Rep + p:Rep + k:Rep + Variety +)
+
